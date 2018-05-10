c69ea7452ccde6c20f15b566595a05e2b26417ae
Edvaldo Santos <edvaldonsantos86@gmail.com>
2018-05-10 10:05:09 -0300

    PrimeiroCommit

---
+++ TrabalhandoComLinqs/TrabalhandoComLinqs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhandoComLinqs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo do Uso do Git Hub");
+            Console.ReadKey();
+        }
+    }
+}
