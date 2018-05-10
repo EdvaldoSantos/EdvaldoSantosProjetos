aa2f3aa787ff620cfffb7418560ded63977654e2
Edvaldo Santos <edvaldonsantos86@gmail.com>
2018-05-10 10:50:58 -0300

    Trabalhando Com Linq

--- TrabalhandoComLinqs/TrabalhandoComLinqs/Program.cs
+++ TrabalhandoComLinqs/TrabalhandoComLinqs/Program.cs
@@ -10,8 +10,70 @@ namespace TrabalhandoComLinqs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Exemplo do Uso do Git Hub");
+            List<string> times = new List<string>();
+            times.Add("São Paulo");
+            times.Add("Atletico Mineiro");
+            times.Add("Atletico Paranaense");
+            times.Add("Cruzeiro");
+            times.Add("Corinthians");
+            times.Add("América Mineiro");
+            times.Add("Santos");
+            times.Add("Palmeiras");
+            times.Add("Bahia");
+            times.Add("Sport ");
+            times.Add("Vitória");
+            times.Add("Paraná");
+            times.Add("Flamengo");
+            times.Add("Fluminese");
+            times.Add("Vasco");
+            times.Add("Botafogo");
+            times.Add("Internacional");
+            times.Add("Gremio");
+            times.Add("Ceará");
+            times.Add("Chapecoense");
+
+            Console.WriteLine(BuscarPrimeiroComForeach(times, "São Paulo"));
+            Console.WriteLine(BuscarPrimeiroComLinq(times, "Chapecoense"));
+            Console.WriteLine(BuscarPrimeiroComExpressaoLambda(times, "Paraná"));
+            BuscarLista(times, "S").ForEach(x => Console.WriteLine(x));
+            BuscarLista(times, "F").ForEach(x => Console.WriteLine(x));
+            BuscarListaLambda(times, "A").ForEach(x => Console.WriteLine(x));
             Console.ReadKey();
+        }
+
+        public static string BuscarPrimeiroComForeach(List<string> lista, string termo)
+        {
+            foreach (string item in lista)
+            {
+                if (item.Equals(termo))
+                    return item;
+            }
+
+            return null;
+        }
+
+        public static string BuscarPrimeiroComLinq(List<string> lista, string termo)
+        {
+            return (from item in lista
+                    where item.Equals(termo)
+                    select item).First();
+        }
+
+        public static string BuscarPrimeiroComExpressaoLambda(List<string> lista, string termo)
+        {
+            return lista.First(x => x.Equals(termo));
+        }
+
+        public static List<string> BuscarLista(List<string> lista, string termo)
+        {
+            return (from item in lista
+                    where item.Contains(termo)
+                    select item).ToList();
+        }
+
+        public static List<string> BuscarListaLambda(List<string> lista, string termo)
+        {
+            return lista.Where(x => x.Contains(termo)).ToList();
         }
     }
 }
